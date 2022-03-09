98212af1cd507537f525c58adc2c9949e3df31a2
Shweta2105 <sghadi21@gmail.com>
2022-03-09 22:45:14 +0530

    orderlist displayed with details

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,28 +1,62 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shoppingapp/providers/orders.dart' as ord;
 import 'package:intl/intl.dart';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   const OrderItem(this.order);
+
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime)),
-            trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text('\$${widget.order.amount}'),
+              subtitle: Text(
+                  DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-          )
-        ],
-      ),
-    );
+            if (_expanded)
+              Container(
+                  height: min(widget.order.products.length * 20.0 + 20, 180),
+                  child: ListView(
+                    children: widget.order.products
+                        .map((prod) => Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  prod.title,
+                                  style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  '${prod.quantity}x \$${prod.price}',
+                                  style: const TextStyle(fontSize: 18),
+                                )
+                              ],
+                            ))
+                        .toList(),
+                  )),
+          ],
+        ));
   }
 }
